db8af352504515e30018d164025d6712f731e2d4
root <Sandrine Ishimwe>
2023-08-26 06:52:43 +0000

    6. Lists of lists = Matrix correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
-        for cols in row:
+        for cols in rows:
             print("{:d}".format(cols), end=" " if cols != rows[-1] else "")
         print()
