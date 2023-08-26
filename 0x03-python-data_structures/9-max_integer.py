53a8528b633326fdea2205ddfef740c3d59c7915
root <Sandrine Ishimwe>
2023-08-26 06:49:54 +0000

    9. Find the max correction

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
     b = my_list[0]
-    for i in range(len(my_list)):
+    for j in range(len(my_list)):
         if my_list[j] > b:
             b = my_list[j]
     return (b)
