aeae0bb2ca666b3b3505befdc160a4cfb5c4875d
root <Sandrine Ishimwe>
2023-09-11 16:20:29 +0000

    pycodestyle on 1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Defines list class MyList."""
+
+
 class MyList(list):
+    """Built-in list class that do Sort."""
+
     def print_sorted(self):
+        """Print a  sorted list in ascending order."""
         print(sorted(self))
