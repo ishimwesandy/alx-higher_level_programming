facdf1dcda48dab800a11cfab5521210313fab7b
root <Sandrine Ishimwe>
2023-09-11 20:28:04 +0000

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Declaring MyInt class  that inherits from int."""
+
+
 class MyInt(int):
+    """ operators == and !=."""
+
     def __eq__(self, value):
+        """Override the opeartor with != behavior."""
         return self.real != value
+
     def __ne__(self, value):
+        """Override the  operator with == behavior."""
         return self.real == value
