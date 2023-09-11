6475f1e2e4b4b5f95f53ef0c5d3ed5af0d80fb6b
root <Sandrine Ishimwe>
2023-09-11 18:16:29 +0000

    pycodestyle on 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,12 +3,7 @@
 
 
 class MyList(list):
-
-
     """ Built-in list class that do Sort. """
-
     def print_sorted(self):
-
-
         """ Print a  sorted list in ascending order. """
         print(sorted(self))
